a6513438b408cdae24cb4370164a2cd15c491c96
Shubham7350 <90132010+Shubham7350@users.noreply.github.com>
2022-08-25 18:48:11 +0530

    get post photos method done

--- methods/Files/FileRoute.js
+++ methods/Files/FileRoute.js
@@ -1,20 +1,19 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const multer = require("multer");
-const {fileUpload} = require("./FileController.js");
-const multerFilesUpload = multer({ dest: 'uploads/' })
+const { fileUpload } = require("./FileController.js");
+const multerFilesUpload = multer({ dest: "uploads/" });
+const posts = require("../../models/posts.js");
 
 const router = Router();
 
-router.post(
-  "/upload",
-  [
-    multerFilesUpload.single("file")
-  ],
-  fileUpload
-);
+router.post("/upload", [multerFilesUpload.single("file")], fileUpload);
 
 // router.get("/images", (req, res) =>{
-//   res.send(multerFilesUpload.Location);
+//   res.send(db.collection('posts').find({photoLink}));
 // })
+router.get("/images", async (req, res) => {
+  const images = await posts.find();
+  return res.send(images);
+});
 
 module.exports.FileRoute = router;
